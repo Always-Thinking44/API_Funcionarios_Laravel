bbcf3ad36c295ee3f94397c84550d411661dc907
Aijá Gourgel <agourgel44@gmail.com>
2026-07-15 06:22:48 +0100

    adicionando visualizar usuarios

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,17 +3,28 @@
 namespace App\Http\Controllers\Api;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index() : JsonResponse
     {
-        $users = User::orderByDesc('id')->get();
+        $users = User::orderByDesc('id')->paginate(2);
 
+        //Retorna os usuários paginados em formato JSON
         return response()->json([
             'status' => 'true',
             'users' => $users,
+        ], 200);
+    }
+
+    public function show(User $user) : JsonResponse
+    {
+        //Retorna o usuário em formato JSON
+        return response()->json([
+            'status' => 'true',
+            'user' => $user,
         ], 200);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']); // GET - 127.0.0.1:8000/api/user?page=1
+Route::get('/user/{user}', [UserController::class, 'show']); // GET - 127.0.0.1:8000/api/user/1
